d088c68557dc080f98d66bbc00a391013d464e37
SARANSH KASLIWAL <123162014+codersarz@users.noreply.github.com>
2024-07-17 18:59:50 +0530

    Update Check whether BST contains Dead End.cpp

--- BST/Check whether BST contains Dead End.cpp
+++ BST/Check whether BST contains Dead End.cpp
@@ -1,5 +1,35 @@
+//Approach-1
+//Time-Complexity->O(n)
+//Space-Complexity->O(n)
+void make(set<int> &leaf,set<int> &nodes,Node* root){
+        if(!root){
+            return;
+        }
+        
+        nodes.insert(root->data);
+        if(root->left==NULL && root->right==NULL){
+            leaf.insert(root->data);
+        }
+        
+        make(leaf,nodes,root->left);
+        make(leaf,nodes,root->right);
+    }
+    bool isDeadEnd(Node *root)
+    {
+        set<int> nodes,leaf;
+        make(leaf,nodes,root);
+        
+        for(auto l:leaf){
+            if(nodes.count(l+1) && (nodes.count(l-1) || l==1)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
 //Optimal-Solution
-//Time-Complexity->O((n)
+//Time-Complexity->O(n)
 //Space-Complexity->O(h)
 bool solve(Node* root,int start,int end){
         if(!root){
