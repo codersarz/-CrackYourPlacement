efd083afe63727862512ba5ff8c361882f3d4109
SARANSH KASLIWAL <123162014+codersarz@users.noreply.github.com>
2024-07-19 20:37:26 +0530

    Update Populating Next Right Pointers in Each Node.cpp

--- Binary Trees/Populating Next Right Pointers in Each Node.cpp
+++ Binary Trees/Populating Next Right Pointers in Each Node.cpp
@@ -19,3 +19,22 @@ Node* connect(Node* root) {
 
         return root;
     }
+//Approach-2 (Using Level Order Traversal)
+Node* connect(Node* root) {
+        if(root==NULL)return root;
+        queue<Node *> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node *t=q.front();
+                q.pop();
+                if(t->left!=NULL)q.push(t->left);
+                if(t->right!=NULL)q.push(t->right);
+                Node *temp=q.front();
+                t->next=temp;
+                if(i==n-1)t->next=NULL;
+            }
+        }
+        return root;
+    }
