4dc88fd3f329a835b8628eb95d30e5da2928b237
SARANSH KASLIWAL <123162014+codersarz@users.noreply.github.com>
2024-07-24 14:41:28 +0530

    Update Number of Provinces.cpp

--- Graphs/Number of Provinces.cpp
+++ Graphs/Number of Provinces.cpp
@@ -23,3 +23,43 @@ int findCircleNum(vector<vector<int>>& isConnected) {
         
         return count;
     }
+//BFS
+void BFS(unordered_map<int,vector<int>> &adj,int u,vector<bool> &vis){
+        queue<int> q;
+        vis[u]=true;
+        q.push(u);
+
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(int &v:adj[curr]){
+                if(!vis[v]){
+                    vis[v]=true;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        int count=0;
+        vector<bool> vis(n,false);
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                BFS(adj,i,vis);
+                count++;
+            }
+        }
+
+        return count;
+    }
