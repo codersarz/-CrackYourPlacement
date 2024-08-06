6979e29e0fa957c6ef813f2890bd47a5bb76640f
SARANSH KASLIWAL <123162014+codersarz@users.noreply.github.com>
2024-08-06 11:36:54 +0530

    Create Course Schedule.cpp

---
+++ Graphs/Course Schedule.cpp
@@ -0,0 +1,41 @@
+bool topologicalSortCheck(unordered_map<int,vector<int>> &adj,int n,vector<int> &indegree){
+        queue<int> q;
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            count++;
+
+            for(auto &v:adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+
+        return count==n;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(numCourses,0);
+
+        for(auto &v:prerequisites){
+            int a=v[0];
+            int b=v[1];
+            //b--->a
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+
+        //if cycle is present not possible since deadlock condition
+        return topologicalSortCheck(adj,numCourses,indegree);
+    }
